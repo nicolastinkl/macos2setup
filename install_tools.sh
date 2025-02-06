e9e7fb0e0f21bdb660d91cdb29bfd60ab2515635
zeus <nicolastinkl@gmail.com>
2025-02-06 16:11:53 +0800

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -9,14 +9,14 @@ pause() {
 }
 
 
-Install Oh My Zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  echo "Installing Oh My Zsh..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-  echo "Oh My Zsh is already installed, skipping."
-fi
-pause
+# Install Oh My Zsh
+# if [ ! -d "$HOME/.oh-my-zsh" ]; then
+#   echo "Installing Oh My Zsh..."
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# else
+#   echo "Oh My Zsh is already installed, skipping."
+# fi
+# pause
 
 
 # Install Homebrew
