fafe7031748d336812f509a9cebbb0af98783f99
Zeus <nicolastinkl@gmail.com>
2026-04-26 16:40:23 +0800

    flutter_macos_3.41.7-stable
    flutter_macos_3.41.7-stable

--- install_tools.sh
+++ install_tools.sh
@@ -90,10 +90,10 @@ pause
 # Install Flutter
 if [ ! -d "$HOME/Developer/flutter" ]; then
   echo "Installing Flutter..."
-  wget  https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.32.3-stable.zip
-  #https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.27.1-stable.zip
-  unzip flutter_macos_3.32.3-stable.zip -d ~/Developer/
-  rm flutter_macos_3.32.3-stable.zip
+  wget  https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.41.7-stable.zip
+  
+  unzip flutter_macos_3.41.7-stable.zip -d ~/Developer/
+  rm flutter_macos_3.41.7-stable.zip
   
 
   echo 'export PATH="$PATH:$HOME/Developer/flutter/bin"' >> ~/.zshenv
